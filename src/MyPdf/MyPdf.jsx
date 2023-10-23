3726653a06f75add51cd75f42ed99a853e8c731f
Mariam <mbadalyan3@gmail.com>
2023-10-23 17:35:11 +0200

    changed styles and changed pdf function

--- src/MyPdf/MyPdf.jsx
+++ src/MyPdf/MyPdf.jsx
@@ -1,33 +1,11 @@
+import React from "react";
 import classes from "./MyPdf.module.css";
 
-const PDF_FILE_URL = "http://localhost:3000/CV_Mariam_Sotova.pdf"; //изменить ссылку на свою страницу
-
 const MyPdf = () => {
-  const downloadFileURL = (url) => {
-    fetch(url)
-      .then((response) => response.blob())
-      .then((blob) => {
-        const blobURL = window.URL.createObjectURL(new Blob([blob]));
-        const fileName = url.split("/").pop();
-        const aTag = document.createElement("a");
-        aTag.href = blobURL;
-        aTag.setAttribute("download", fileName);
-        document.body.appendChild(aTag);
-        aTag.click();
-        aTag.remove();
-      });
-  };
-
   return (
     <div className={classes.ButtonRainbow}>
       <div className={classes.File}>
-        <button
-          onClick={() => {
-            downloadFileURL(PDF_FILE_URL);
-          }}
-        >
-          download resume
-        </button>
+        <button onClick={downloadFileURL}>download CV</button>
         <span></span>
         <span></span>
       </div>
@@ -36,3 +14,23 @@ const MyPdf = () => {
 };
 
 export default MyPdf;
+
+// private
+
+const PDF_FILE_URL = "http://localhost:3000/CV_Mariam_Sotova.pdf"; //изменить ссылку на свою страницу
+
+function downloadFileURL() {
+  const url = PDF_FILE_URL;
+  fetch(url)
+    .then((response) => response.blob())
+    .then((blob) => {
+      const blobURL = window.URL.createObjectURL(new Blob([blob]));
+      const fileName = url.split("/").pop();
+      const aTag = document.createElement("a");
+      aTag.href = blobURL;
+      aTag.setAttribute("download", fileName);
+      document.body.appendChild(aTag);
+      aTag.click();
+      aTag.remove();
+    });
+}
